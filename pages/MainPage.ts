4a29b99a8ed9ec26788a7acc29e69a444d241c46
evanjscallan <evanjscallan@gmail.com>
2023-08-03 16:08:11 -0500

    clearned up unnecessary files

--- pages/MainPage.ts
+++ pages/MainPage.ts
@@ -1,11 +1,9 @@
 import { type Locator, type Page} from '@playwright/test';
 export class MainPage {
     readonly page: Page
-    readonly tableBody: Locator;
     readonly playButton: Locator;
     constructor(page: Page) {
         this.page = page
-        this.tableBody = page.locator('table#game_board tbody')
         this.playButton = page.locator('a:has-text("PLAY")').first()
     }
     async navToPage() {
@@ -13,8 +11,8 @@ export class MainPage {
         await this.playButton.click()
     }
     async cardSelector() {
-        let firstCard;
-        let lastCard;
+        let firstCard: Locator;
+        let lastCard: Locator;
         const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
         for (let number = 1; number < 16; number++) {
             //cards below 10 have a 0 in the first digit place (e.g. 01, 02...)
